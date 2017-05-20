f3007d915ddb6de5896f30fd1945da8ea01a7b85
Marina <marinancarrizo@gmail.com>
2017-05-20 04:15:48 -0300

    Agregado pagina catalogo con listado de libros por genero (mejorar y revisar paginado)

--- webVentaLibros/Controllers/CatalogoController.cs
+++ webVentaLibros/Controllers/CatalogoController.cs
@@ -22,8 +22,36 @@ namespace webVentaLibros.Controllers
                                     idGenero = genero.idGenero,
                                     nombreGenero = genero.nombre
                                 }).ToList();
-            return View(listaGeneros);
+            ViewBag.listadoGeneros = listaGeneros;
+            return View();
         }
 
+
+        public ActionResult MostrarLibros(int idGenero)
+        {
+            var bd = new bdVentaLibrosDataContext();
+
+            var listaLibros = (from libro in bd.Libros
+                               where libro.idGenero == idGenero
+                               select new LibroModel
+                               {
+                                   codigoBarra = libro.codigoBarra,
+                                   titulo = libro.titulo,
+                                   foto = libro.foto,
+                                   precio = Convert.ToDouble(libro.precio)
+                               }).ToList();
+
+            var listaGeneros = (from genero in bd.Generos
+                                select new GeneroModel
+                                {
+                                    idGenero = genero.idGenero,
+                                    nombreGenero = genero.nombre
+                                }).ToList();
+
+            ViewBag.listadoGeneros = listaGeneros;
+            ViewBag.listadoLibros = listaLibros;
+
+            return View();
+        }
     }
 }
