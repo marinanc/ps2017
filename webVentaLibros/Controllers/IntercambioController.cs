d4c22863fac2a0c718661ccf60288f75e689c465
Marina <marinancarrizo@gmail.com>
2017-06-04 18:52:54 -0300

    Agregado solamente la pagina principal de Intercambios (solo encabezado y pie)

--- webVentaLibros/Controllers/IntercambioController.cs
+++ webVentaLibros/Controllers/IntercambioController.cs
@@ -11,7 +11,7 @@ namespace webVentaLibros.Controllers
         //
         // GET: /Intercambio/
 
-        public ActionResult Index()
+        public ActionResult Intercambios()
         {
             return View();
         }
