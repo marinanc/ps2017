1f532fb44d9c6912526675dce20ce837df065fc8
Marina <marinancarrizo@gmail.com>
2017-06-04 03:36:15 -0300

    Agregado modificar libro

--- webVentaLibros/Controllers/AdminController.cs
+++ webVentaLibros/Controllers/AdminController.cs
@@ -67,34 +67,7 @@ namespace webVentaLibros.Controllers
                                    editorial = libro.Editoriales.nombre,
                                    precio = Convert.ToDouble(libro.precio)
                                }).ToList();
-            /**
-            var listaLibros = (from libro in bd.Libros
-                               join editorial in bd.Editoriales on libro.idEditorial equals editorial.idEditorial                           
-                               select libro).ToList();
-            **/
-            /**
-            var listaLibros = (from libro in bd.Libros
-                               from editorial in bd.Editoriales
-                               from autor in bd.Autores
-                               from genero in bd.Generos
-                               where libro.idEditorial == editorial.idEditorial &&
-                               libro.idGenero == genero.idGenero &&
-                               libro.idAutor1 == autor.idAutor &&
-                               libro.idAutor2 == autor.idAutor &&
-                               libro.idAutor3 == autor.idAutor &&
-                               libro.idAutor4 == autor.idAutor
-                               select new LibroModel { 
-                               codigoBarra = libro.codigoBarra,
-                               titulo = libro.titulo,
-                               genero = libro.Generos.nombre,
-                               autor1 = libro.Autores.apellidos+", "+libro.Autores.nombres,
-                               autor2 = libro.Autores1.apellidos + ", " + libro.Autores.nombres,
-                               autor3 = libro.Autores2.apellidos + ", " + libro.Autores.nombres,
-                               autor4 = libro.Autores3.apellidos + ", " + libro.Autores.nombres,
-                               editorial = libro.Editoriales.nombre,
-                               precio = Convert.ToDouble(libro.precio)
-                               }).ToList();
-            **/
+            
             ViewBag.listadoAutores = listaAutores;
             ViewBag.listadoGeneros = listaGeneros;
             ViewBag.listadoEditoriales = listaEditoriales;
@@ -143,6 +116,130 @@ namespace webVentaLibros.Controllers
                 TempData["Message"] = "Libro agregado!";
             }
 
+            return RedirectToAction("AgregarLibro");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrador")]
+        public ActionResult ModificarLibro(string codBarra)
+        {
+            var bd = new bdVentaLibrosDataContext();
+            
+            var listaGeneros = ((from genero in bd.Generos
+                                 select new GeneroModel
+                                 {
+                                     idGenero = genero.idGenero,
+                                     nombreGenero = genero.nombre
+                                 }).OrderBy(g => g.nombreGenero)).ToList();
+
+            var listaAutores = ((from autor in bd.Autores
+                                 select new AutorModel
+                                 {
+                                     idAutor = autor.idAutor,
+                                     nombreAutor = autor.nombres,
+                                     apellidoAutor = autor.apellidos
+                                 }).OrderBy(a => a.apellidoAutor)).ToList();
+
+            var listaEditoriales = ((from editorial in bd.Editoriales
+                                     select new EditorialModel
+                                     {
+                                         idEditorial = editorial.idEditorial,
+                                         nombre = editorial.nombre
+                                     }).OrderBy(e => e.nombre)).ToList();
+            
+            ViewBag.listadoAutores = listaAutores;
+            ViewBag.listadoGeneros = listaGeneros;
+            ViewBag.listadoEditoriales = listaEditoriales;
+
+            var libroElegido = from libro in bd.Libros
+                               where codBarra == libro.codigoBarra
+                               from editorial in bd.Editoriales
+                               .Where(e => e.idEditorial == libro.idEditorial).DefaultIfEmpty()
+                               from genero in bd.Generos
+                               .Where(g => g.idGenero == libro.idGenero).DefaultIfEmpty()
+                               from autor in bd.Autores
+                               .Where(a => a.idAutor == libro.idAutor1).DefaultIfEmpty()
+                               .Where(a => a.idAutor == libro.idAutor2).DefaultIfEmpty()
+                               .Where(a => a.idAutor == libro.idAutor3).DefaultIfEmpty()
+                               .Where(a => a.idAutor == libro.idAutor4).DefaultIfEmpty()
+                               select new LibroModel{
+                                   codigoBarra = libro.codigoBarra,
+                                   titulo = libro.titulo,
+                                   idAutor1 = libro.Autores.idAutor,
+                                   idAutor2 = libro.Autores1.idAutor,
+                                   idAutor3 = libro.Autores2.idAutor,
+                                   idAutor4 = libro.Autores3.idAutor,
+                                   autor1 = libro.Autores.apellidos + ", " + libro.Autores.nombres,
+                                   autor2 = libro.Autores1.apellidos + ", " + libro.Autores.nombres,
+                                   autor3 = libro.Autores2.apellidos + ", " + libro.Autores.nombres,
+                                   autor4 = libro.Autores3.apellidos + ", " + libro.Autores.nombres,
+                                   idGenero = libro.Generos.idGenero,
+                                   genero = libro.Generos.nombre,
+                                   idEditorial = libro.Editoriales.idEditorial,
+                                   editorial = libro.Editoriales.nombre,
+                                   sinopsis = libro.sinopsis,
+                                   precio = Convert.ToDouble(libro.precio),
+                                   paginas = libro.paginas,
+                                   foto = libro.foto,
+                                   edicion = libro.edicion
+                               };
+
+            return View(libroElegido);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        [HttpPost]
+        public ActionResult ModificarLibro(LibroModel libroElegido, HttpPostedFileBase foto)
+        {
+            var bd = new bdVentaLibrosDataContext();
+
+            if (foto != null) { 
+                foto.SaveAs(System.IO.Path.Combine(@"D:\webVentaLibros\webVentaLibros\img\catalogo", System.IO.Path.GetFileName(foto.FileName)));
+            }
+
+            var libroModificado = from libro in bd.Libros
+                                  where libroElegido.codigoBarra == libro.codigoBarra
+                                  select libro;                     
+
+            //Cambio los datos del inmueble
+            if (foto != null)
+            {
+                foreach (var libro in libroModificado)
+                {
+                    libro.titulo = libroElegido.titulo;
+                    libro.idGenero = libroElegido.idGenero;
+                    libro.idEditorial = libroElegido.idEditorial;
+                    libro.idAutor1 = libroElegido.idAutor1;
+                    libro.idAutor2 = libroElegido.idAutor2;
+                    libro.idAutor3 = libroElegido.idAutor3;
+                    libro.idAutor4 = libroElegido.idAutor4;
+                    libro.paginas = libroElegido.paginas;
+                    libro.precio = Convert.ToDecimal(libroElegido.precio);
+                    libro.sinopsis = libroElegido.sinopsis;
+                    libro.edicion = libroElegido.edicion;
+                    libro.foto = @"img/catalogo/" + foto.FileName;
+                }
+            }
+            else
+            {
+                foreach (var libro in libroModificado)
+                {
+                    libro.titulo = libroElegido.titulo;
+                    libro.idGenero = libroElegido.idGenero;
+                    libro.idEditorial = libroElegido.idEditorial;
+                    libro.idAutor1 = libroElegido.idAutor1;
+                    libro.idAutor2 = libroElegido.idAutor2;
+                    libro.idAutor3 = libroElegido.idAutor3;
+                    libro.idAutor4 = libroElegido.idAutor4;
+                    libro.paginas = libroElegido.paginas;
+                    libro.precio = Convert.ToDecimal(libroElegido.precio);
+                    libro.sinopsis = libroElegido.sinopsis;
+                    libro.edicion = libroElegido.edicion;
+                }
+            }
+            //Hago el submit
+            bd.SubmitChanges();
+            TempData["Message"] = "Libro modificado!";
             return RedirectToAction("AgregarLibro");
         }
 
---
+++ webVentaLibros/Controllers/IntercambioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webVentaLibros.Controllers
+{
+    public class IntercambioController : Controller
+    {
+        //
+        // GET: /Intercambio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
